9a3034337866578510da5c4762c99e5a8433f2de
Zyad Dev <117089930+zyad87@users.noreply.github.com>
2024-09-09 14:41:23 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+function summation(x, y) {
+  return x + y;
+}
+function subtraction(x, y) {
+  return x - y;
+}
+function multiplcation(x, y) {
+  return x * y;
+}
+function division(x, y) {
+  return x / y;
+}
+
+var multiplcationResult = (x, y) => {
+  return x % y;
+};
+
+console.log(summation(1, 2));
+console.log(subtraction(1, 2));
+console.log(multiplcationResult(1, 2));
+console.log(multiplcationResult(1, 2));
